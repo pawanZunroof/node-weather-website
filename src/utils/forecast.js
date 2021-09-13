c99469cf2e53566064dcacb037478a8c6286c5bc
Pawan Singhla <pawan.singhla@zunroof.com>
2021-09-13 15:32:51 +0530

    update forecast details

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -12,7 +12,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.location.name + ' It is currently ' + body.current.weather_descriptions[0] + ' degress out. There is a ' + body.current.precip + '% chance of rain.')
+            callback(undefined, body.location.name + ' It is currently ' + body.current.weather_descriptions[0] + ' degress out. There is a ' + body.current.precip + '% chance of rain.' + "current humidity is " + body.current.humidity);
         }
     })
 }
